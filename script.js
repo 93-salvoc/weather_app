f0b238f84833791e1d91d32028519026ea4e26ae
JohannaKoellner <johanna.a.koellner@gmail.com>
2023-02-05 23:27:53 +0100

    set up fetch & linked to html file (still messy)

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-const apiKey = "2431bb7af7d54949a0e180334231801";
-
-const city = document.querySelector(".city");
-const icon = document.querySelector(".icon");
-const condition = document.querySelector(".condition");
-const temp = document.querySelector(".temp");
-
-const cityInput = document.querySelector(".city-input");
-const searchForm = document.querySelector("form");
-
-function setFavicons(favImg) {
-    let favicon = document.querySelector('.favicon');
-    favicon.href = favImg
-}
 
 
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    fetch(
-            `https://api.unsplash.com/search/photos/?client_id=2kd3ZjVt5tGBAlH0KMTER7YQwBnVlRIImLRYgoD3yPM&query=${cityInput.value}`
-        )
+const searchForm = document.querySelector(".search-box");
+
+searchForm.addEventListener('submit', (getWeatherAndImg) => {
+    getWeatherAndImg.preventDefault(); //to prevent this function from running right away when the site is called (this would be default with "submit")
+    
+    //get input and save in city variable
+    const city = document.getElementById("city-home").value;
+    console.log(city);
+
+    //update html - template
+    document.body.innerHTML = 
+        `Yikes, I ended up in javascript :O`;
+    
+    //fetch img based on input
+    fetch(`https://api.unsplash.com/search/photos/?client_id=2kd3ZjVt5tGBAlH0KMTER7YQwBnVlRIImLRYgoD3yPM&query=${city}`) //Is it ok to use Abed's key here?
         .then((response) => response.json())
         .then((data) => {
-            let randomIndex = Math.floor(Math.random() * data.results.length);
-            console.log(data.results);
-            console.log(data.results[randomIndex].urls.regular);
-            document.body.style.backgroundImage = `url(${data.results[randomIndex].urls.regular})`;
+          let randomIndex = Math.floor(Math.random() * data.results.length);
+          const imgURL = data.results[randomIndex].urls.regular;
+          console.log(imgURL);
+          //set img as background of #weather-layout
+          document.body.style.backgroundImage = `url(${imgURL})`; //fix this
         });
 
-    fetch(
-            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput.value}`
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
+    //fetch weather based on input
 
-            if (data.current.condition.text === "Partly cloudy") {
-                setFavicons('./icons/partly-cloudy.ico');
+    //extract the weather data
 
-            } else {
-                setFavicons('./icons/default.gif');
+    //fill in missing html
 
-            }
-            city.innerText = data.location.name;
-            icon.src = data.current.condition.icon;
-            condition.innerText = data.current.condition.text;
-            temp.innerText = data.current.temp_c + "°C";
-        })
-        .catch((err) => console.error(err));
-});
+})
+
+//const currentWeatherURL = `https://api.weatherapi.com/v1/current.json?key=c1c493a1ed79402aa6090757230402&q=${city}`
+// const futureWeatherURL = `https://api.weatherapi.com/v1/forecast.json?key=c1c493a1ed79402aa6090757230402&q=${city}&days=3`;
+// const pastWeatherURL = `https://api.weatherapi.com/v1/history.json?key=c1c493a1ed79402aa6090757230402&q=${city}&days=3`;
+
+// fetch(futureWeatherURL)
+//     .then(response => response.json())
+//     .then((weather) => {
+//         console.log(weather);
+//         //document.createElement("p") = data.location.name;
+//         //document.body.appendChild
+//         // icon.src = data.current.condition.icon;
+//         // condition.innerText = data.current.condition.text;
+//         // temp.innerText = data.current.temp_c + "°C";
+//     })
