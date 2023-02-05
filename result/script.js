23dbc59e0137c38414b360842b2b02ba259e7681
Salvatore <salvatorecantarella99@gmail.com>
2023-02-05 14:09:12 +0000

    first commit with working home but not linked yet

--- result/script.js
+++ result/script.js
@@ -10,29 +10,31 @@ const searchForm = document.querySelector("form");
 
 
 
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    fetch(
-            `https://api.unsplash.com/search/photos/?client_id=2kd3ZjVt5tGBAlH0KMTER7YQwBnVlRIImLRYgoD3yPM&query=${cityInput.value}`
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            let randomIndex = Math.floor(Math.random() * data.results.length);
-            console.log(data.results);
-            console.log(data.results[randomIndex].urls.regular);
-            document.body.style.backgroundImage = `url(${data.results[randomIndex].urls.regular})`;
-        });
+function submitForm() {
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        fetch(
+                `https://api.unsplash.com/search/photos/?client_id=2kd3ZjVt5tGBAlH0KMTER7YQwBnVlRIImLRYgoD3yPM&query=${cityInput.value}`
+            )
+            .then((response) => response.json())
+            .then((data) => {
+                let randomIndex = Math.floor(Math.random() * data.results.length);
+                console.log(data.results);
+                console.log(data.results[randomIndex].urls.regular);
+                document.body.style.backgroundImage = `url(${data.results[randomIndex].urls.regular})`;
+            });
 
-    fetch(
-            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput.value}`
-        )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            city.innerText = data.location.name;
-            icon.src = data.current.condition.icon;
-            condition.innerText = data.current.condition.text;
-            temp.innerText = data.current.temp_c + "°C";
-        })
-        .catch((err) => console.error(err));
-});
+        fetch(
+                `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput.value}`
+            )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                city.innerText = data.location.name;
+                icon.src = data.current.condition.icon;
+                condition.innerText = data.current.condition.text;
+                temp.innerText = data.current.temp_c + "°C";
+            })
+            .catch((err) => console.error(err));
+    });
+}
